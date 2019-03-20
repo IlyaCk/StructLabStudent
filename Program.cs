cbb278703a94370abbf8a283608f4af92a312ed8
IlyaCk <ilya.porublyov@gmail.com>
2019-03-20 15:50:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            // TODO   implement this method.
+            // It should read the file whose fileName has been passed and fill 
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            // TODO   implement this method
+            // It should call method(s) for concrete variant(s)
+        }
+
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("input.txt");
+            runMenu(studs);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+
+        public Student(string lineWithAllData)
+        {
+            // TODO   you SHOULD IMPLEMENT constructor with exactly this signature
+            // lineWithAllData is string contating all data about one student, as described in statement
+        }
+    }
+}
