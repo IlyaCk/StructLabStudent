758f71dc491aa4190fe836f177078f81e39e9f1c
IlyaCk <ilya.porublyov@gmail.com>
2023-05-07 01:02:48 +0300

    Allow use either array or List

--- Program.cs
+++ Program.cs
@@ -9,16 +9,18 @@ namespace struct_lab_student
     partial class Program
     {
         static Student[] ReadData(string fileName)
+        // You may (if want) change ``Student[]'' to ``List<Student>''
         {
             // TODO   implement this method.
             // It should read the file whose fileName has been passed and fill data inside array of Student
         }
 
         static void runMenu(Student[] studs)
+        // You may (if want) change ``Student[]'' to ``List<Student>''
         {
             // TODO   implement this method
             // It should call method(s) for specific variant(s).
-            // Arrays studs should already contain data (probably filled with ReadData method)
+            // Array (or List) studs should already contain data (probably filled with ReadData method)
         }
 
         static void Main(string[] args)
